57c124143203b8b0939555714475b6e2c4ffbdae
leathermain <=>
2022-03-13 22:59:44 +0200

    fix code

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,8 +1,13 @@
 extern crate open;
 
+#[tauri::command]
+fn open_website(link: String) {
+  open::that(link).unwrap();
+}
+
 fn main() {
-  
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![open_website])
     .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    .expect("failed to run app");
 }
