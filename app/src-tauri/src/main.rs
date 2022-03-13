69581e879b50a8faf31328791562d36a52d71655
leathermain <=>
2022-03-13 22:20:25 +0200

    test compile

--- app/src-tauri/src/main.rs
+++ app/src-tauri/src/main.rs
@@ -1,13 +1,8 @@
 extern crate open;
 
-#[tauri::command]
-fn open_website(link: String) {
-  open::that(link).unwrap();
-}
-
 fn main() {
+  
   tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![open_website])
     .run(tauri::generate_context!())
-    .expect("failed to run app");
+    .expect("error while running tauri application");
 }
